276ce56e5b022e8e108c48655c1f79fcb70a3a50
hinata@jr <chgeethakrishna@gmail.com>
2022-08-10 10:40:15 +0530

    pagination bug and listgroup generalized

--- src/components/commons/listGroup.jsx
+++ src/components/commons/listGroup.jsx
@@ -2,16 +2,15 @@ import React, { Component } from "react";
 
 class ListGroup extends Component {
   render() {
-    const { items, selected, onSelect } = this.props;
-    console.log(items);
+    const { items, selected, onSelect, idProp, textProp } = this.props;
     return (
       <div>
         <ul className="list-group">
           <li className={"list-group-item" + (selected === "all" ? " active" : "")} onClick={() => onSelect("all")}>All</li>
           {
             items.map(item => {
-              return (<li key={item._id} className={"list-group-item" + (selected === item.name ? " active" : "")} onClick={() => onSelect(item.name)}>
-                {item.name}
+              return (<li key={item[idProp]} className={"list-group-item" + (selected === item[textProp] ? " active" : "")} onClick={() => onSelect(item.name)}>
+                {item[textProp]}
               </li>)
             })
           }
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,22 +5,30 @@ import ListGroup from './commons/listGroup';
 import { getMovies } from '../services/fakeMovieService';
 import { getGenres } from '../services/fakeGenreService';
 import { paginate } from '../utils/paginate';
-import {filter} from '../utils/filter';
+import { filter } from '../utils/filter';
 
 class Movies extends Component {
     state = {
-        movies: getMovies(),
-        genres: getGenres(),
+        movies: [],
+        genres: [],
         currentPage: 1,
         pageSize: 5,
         selectedGenre: "all"
     }
-    handleDelete = (movieId) => {
+
+    componentDidMount() {
+        this.setState({
+            movies: getMovies(),
+            genres: getGenres()
+        })
+    }
+
+    handleRemoveMovie = (movieId) => {
         this.setState({
             movies: this.state.movies.filter(movie => movie._id !== movieId)
         });
     }
-    changeLiked = (id) => {
+    handleMovieChangeLiked = (id) => {
         const movies = this.state.movies;
         const movie = movies.find((movie) => movie._id === id);
         movie.liked = !movie.liked;
@@ -28,14 +36,15 @@ class Movies extends Component {
             movies: movies
         })
     }
-    changePage = (page) => {
+    handlePageChange = (page) => {
         this.setState({
             currentPage: page
         });
     }
-    chnageGenre = (genre) => {
+    handleGenreChange = (genre) => {
         this.setState({
-            selectedGenre: genre
+            selectedGenre: genre,
+            currentPage: 1
         })
     }
     // prepareMovies = () => {
@@ -52,16 +61,17 @@ class Movies extends Component {
 
     render() {
         let movies = filter(this.state.movies, 'genre.name', this.state.selectedGenre);
+        const dataSize = movies.length;
         movies = paginate(movies, this.state.currentPage, this.state.pageSize);
         return (
             <>
                 {this.state.movies.length === 0 && <h4>No movies to show</h4>}
                 {this.state.movies.length > 0 &&
                     <div className='row'>
-                        <div className="col-3">
-                            <ListGroup items={this.state.genres} selected={this.state.selectedGenre} onSelect={this.chnageGenre} />
+                        <div className="col-2">
+                            <ListGroup items={this.state.genres} selected={this.state.selectedGenre} onSelect={this.handleGenreChange} textProp="name" idProp="_id" />
                         </div>
-                        <div className="col-9">
+                        <div className="col">
 
                             {/* <h5>Showing {start}-{end} of {this.state.movies.length} movies in database.</h5> */}
 
@@ -72,8 +82,8 @@ class Movies extends Component {
                                     <tr>
                                         <th>Title</th>
                                         <th>Genre</th>
-                                        <th>InStock</th>
-                                        <th>Price Rate</th>
+                                        <th className='text-center'>InStock</th>
+                                        <th className='text-center'>Price Rate</th>
                                         <th></th>
                                         <th></th>
                                     </tr>
@@ -84,16 +94,16 @@ class Movies extends Component {
                                             <tr key={movie._id}>
                                                 <td>{movie.title}</td>
                                                 <td>{movie.genre.name}</td>
-                                                <td>{movie.numberInStock}</td>
-                                                <td>{movie.dailyRentalRate}</td>
-                                                <td><Like id={movie._id} liked={movie.liked} changeLiked={this.changeLiked} /></td>
-                                                <td><button className='btn btn-warning btn-sm' onClick={() => this.handleDelete(movie._id)}>Remove</button></td>
+                                                <td className='text-center'>{movie.numberInStock}</td>
+                                                <td className='text-center'>{movie.dailyRentalRate}</td>
+                                                <td className='text-center'><Like id={movie._id} liked={movie.liked} changeLiked={this.handleMovieChangeLiked} /></td>
+                                                <td className='text-center'><button className='btn btn-warning btn-sm' onClick={() => this.handleRemoveMovie(movie._id)}>Remove</button></td>
                                             </tr>
                                         )
                                     })}
                                 </tbody>
                             </table>
-                            <Pagination pageSize={this.state.pageSize} dataSize={this.state.movies.length} currentPage={this.state.currentPage} onPageChange={this.changePage} />
+                            <Pagination pageSize={this.state.pageSize} dataSize={dataSize} currentPage={this.state.currentPage} onPageChange={this.handlePageChange} />
                         </div>
                     </div>
                 }
